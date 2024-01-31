d1d53ace7b0c917acfe004c7dd296f469a468dcc
Pedrofigueiredo23 <pedro_f23@hotmail.com>
2024-01-31 19:07:52 +0000

    feat: Forma de pagamento Class

--- src/FormaDePagamento.java
+++ src/FormaDePagamento.java
@@ -2,4 +2,16 @@ public class FormaDePagamento {
     public static void processarPagamento(){
         System.out.println("Processamento de pagamento.");
     }
+
+    class CartaoCredito extends FormaDePagamento{ // subclass Cartao de Crédito
+        public static void processarPagamento(){
+            System.out.println("Pagamento por Cartão de Crédito.");
+        }
+    }
+
+    class PayPal extends FormaDePagamento{ // subclass PayPal
+        public static void processarPagamento(){
+            System.out.println("Pagamento por PayPal.");
+        }
+    }
 }
